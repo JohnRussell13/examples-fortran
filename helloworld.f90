f3b8fd7c421b72a66495ba13458480537898a00a
JohnRussell13 <aleksandar.g1999@gmail.com>
2023-10-26 22:42:19 +0200

    Types

--- helloworld.f90
+++ helloworld.f90
@@ -1,4 +1,20 @@
 program hello
-      ! comment
-      print *, 'Hello, World!'
+      implicit none
+
+      real :: height
+      character :: initial
+      complex :: point
+      logical :: bool
+      integer :: age
+
+      height = 1.82
+      initial = 'A'
+      point = (2, -1)
+      bool = .true.
+      age = 90
+
+      print *, 'Hey ', initial, ', I see it is', bool, 'that you are', height, 'm tall'
+      print *, 'Are you really,', age, 'years old? This is a number', point
+
+      print *, (0, 1), '*', (0,1), '=', (0,1) * (0,1)
 end program hello
